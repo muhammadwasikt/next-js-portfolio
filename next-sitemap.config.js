9c1c0d072e6fe5984016dc584f3e75a9138e8179
muhammadwasikt <wasikhatri11@gmail.com>
2025-03-05 04:55:30 +0500

    site

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://muhammadwasiportfolio.vercel.app',
+  generateRobotsTxt: true, 
+  sitemapSize: 5000,
+};
+
+module.exports = config;
