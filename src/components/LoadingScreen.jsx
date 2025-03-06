348c27c748ddf16388ebf21e691c18efceb74c48
muhammadwasikt <wasikhatri11@gmail.com>
2025-03-06 06:33:01 +0500

    load

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,30 +1,37 @@
-'use client'
-import { useState, useEffect } from 'react'
-import {motion } from 'framer-motion'
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 const LoadingScreen = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
-    const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    const handleLoad = () => setIsLoading(false);
 
-    useEffect(() => {
-        const loadingTime = setTimeout(() => {
-            setIsLoading(false)
-        }, 3000)
-        return () => {
-            clearTimeout(loadingTime)
-        }
-    }, [isLoading])
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
 
-    return (
-        <div>
-            {isLoading ? <div className='h-screen flex justify-center items-center p-2'>
-                <motion.div
-                    className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full"
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
-                /></div> : children}
-        </div>
-    )
-}
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
-export default LoadingScreen
+  return isLoading ? (
+    <div className="h-screen flex justify-center items-center p-2 bg-white">
+      <motion.div
+        className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full"
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+      />
+    </div>
+  ) : (
+    children
+  );
+};
+
+export default LoadingScreen;
