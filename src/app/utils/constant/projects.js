5f39a97df3ac45878d3dd773dbe7fbd55bc71bb9
Muhammad Wasi <156829042+muhammadwasikt@users.noreply.github.com>
2025-10-21 09:53:42 +0500

    Add new projects to the projects list

--- src/app/utils/constant/projects.js
+++ src/app/utils/constant/projects.js
@@ -1,48 +1,59 @@
 const projects = [
-    {
-      title: "BlogBusters - MERN Stack Blog App",
-      description: "A full-stack blog application built with the MERN stack, featuring authentication, CRUD operations, and a sleek UI.",
-      image: "/assets/images/blog-busters.jpg",
-      link: "https://github.com/muhammadwasikt/mern-stack-blog-app"
-    },
-    {
-      title: "Shopping Cart",
-      description: "A simple yet functional shopping cart built with React, providing add-to-cart functionality and real-time updates.",
-      image: "/assets/images/shopping-carts.jpg",
-      link: "https://github.com/muhammadwasikt/shopping-cart"
-    },
-    {
-      title: "Mock Storage",
-      description: "A storage management system built with React and Node.js, allowing users to store and manage their files efficiently.",
-      image: "/assets/images/mock-storage.jpg",
-      link: "https://github.com/muhammadwasikt/Mock-storage"
-    },
-    {
-      title: "Next.js Dashboard",
-      description: "A modern and responsive admin dashboard built with Next.js, Tailwind CSS, and ShadCN UI.",
-      image: "/assets/images/next-dashboard-2.jpg",
-      link: "https://github.com/muhammadwasikt/dashboard-next.js"
-    },
-    {
-      title: "Morent - Car Rental Service",
-      description: "A car rental website featuring booking functionalities and a sleek user interface, built with HTML 5 , CSS 3 and JavaScript.",
-      image: "/assets/images/morrent-rental.jpg",
-      link: "https://github.com/muhammadwasikt/Morent-rental"
-    },
-    {
-      title: "Final Hackathon Project (SMIT)",
-      description: "A collaborative project developed for a hackathon, showcasing innovative solutions using modern web technologies.",
-      image: "/assets/images/next-dashboard.jpg",
-      link: "https://github.com/muhammadwasikt/final-hackhathon-smit"
-    },
-    {
-      title: "Chawk Bazaar",
-      description: "A mini-project built for a hackathon challenge, demonstrating strong problem-solving and development skills.",
-      image: "/assets/images/chawk-bazaar.jpg",
-      link: "https://github.com/muhammadwasikt/mini-hackthon-react"
-    }
-  ];
-  
-  
-  export default projects;
-  
+  {
+    title: "Eduhustle - Blogging Platform",
+    description: "A learning and career growth platform that helps users develop digital skills through practical blogs, tutorials, and productivity guides.",
+    image: "/assets/images/eduhustle.jpg",
+    link: "https://github.com/muhammadwasikt/eduhustle"
+  },
+  {
+    title: "Postoria - Guest Posting Platform",
+    description: "A modern guest-posting and blogging platform where users can publish SEO-optimized articles, explore trending topics, and share knowledge with a global audience.",
+    image: "/assets/images/postoria.jpg",
+    link: "https://github.com/muhammadwasikt/postoria"
+  },
+  {
+    title: "BlogBusters - MERN Stack Blog App",
+    description: "A full-stack blog application built with the MERN stack, featuring authentication, CRUD operations, and a sleek UI.",
+    image: "/assets/images/blog-busters.jpg",
+    link: "https://github.com/muhammadwasikt/mern-stack-blog-app"
+  },
+  {
+    title: "Shopping Cart",
+    description: "A simple yet functional shopping cart built with React, providing add-to-cart functionality and real-time updates.",
+    image: "/assets/images/shopping-carts.jpg",
+    link: "https://github.com/muhammadwasikt/shopping-cart"
+  },
+  {
+    title: "Mock Storage",
+    description: "A storage management system built with React and Node.js, allowing users to store and manage their files efficiently.",
+    image: "/assets/images/mock-storage.jpg",
+    link: "https://github.com/muhammadwasikt/Mock-storage"
+  },
+  {
+    title: "Next.js Dashboard",
+    description: "A modern and responsive admin dashboard built with Next.js, Tailwind CSS, and ShadCN UI.",
+    image: "/assets/images/next-dashboard-2.jpg",
+    link: "https://github.com/muhammadwasikt/dashboard-next.js"
+  },
+  {
+    title: "Morent - Car Rental Service",
+    description: "A car rental website featuring booking functionalities and a sleek user interface, built with HTML 5 , CSS 3 and JavaScript.",
+    image: "/assets/images/morrent-rental.jpg",
+    link: "https://github.com/muhammadwasikt/Morent-rental"
+  },
+  {
+    title: "Final Hackathon Project (SMIT)",
+    description: "A collaborative project developed for a hackathon, showcasing innovative solutions using modern web technologies.",
+    image: "/assets/images/next-dashboard.jpg",
+    link: "https://github.com/muhammadwasikt/final-hackhathon-smit"
+  },
+  {
+    title: "Chawk Bazaar",
+    description: "A mini-project built for a hackathon challenge, demonstrating strong problem-solving and development skills.",
+    image: "/assets/images/chawk-bazaar.jpg",
+    link: "https://github.com/muhammadwasikt/mini-hackthon-react"
+  }
+];
+
+
+export default projects;
